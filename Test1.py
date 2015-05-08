77a4a8be1eaffb0d56e949f6356051a24e7c6b4e
katherinemurphy <16kamurphy@iaa.edu.ec>
2015-05-08 08:30:56 -0500

    Create Test1.py

---
+++ Test1.py
@@ -0,0 +1,10 @@
+Operand1= 95
+Operand2= 64.5 
+
+print "95 + 64.5" = Operand1 + Operand2
+print "95 - 64.5" = Operand1 - Operand2
+print "95 * 64.5" = Operand1 * Operand2
+print "95 / 64.5" = Operand1 / Operand2
+print "95 % 64.5" = Operand1 % Operand2
+
+# My solution uses variables to print the answer. 
