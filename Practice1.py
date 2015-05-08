6566438053d99369211264df9868cc265d8cfbe2
katherinemurphy <16kamurphy@iaa.edu.ec>
2015-05-08 08:26:41 -0500

    Create Practice1.py

---
+++ Practice1.py
@@ -0,0 +1,16 @@
+#Practice # 1 
+#Create 5 variables that store your personal info 
+#Name, Last name, Address, Email, Phone number
+#Print them seperately and then concatenate them in a string 
+
+name = 'Katherine Murphy'
+age = 17 
+gpa = 3.7
+
+print name 
+print age
+print gpa 
+
+print 'My name is', name, ' . I am ' , age , 'years old.' 
+
+print "In ten years, I will be" , age + 10
