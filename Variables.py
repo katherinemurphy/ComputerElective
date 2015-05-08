3f3ee58b00346341c71248453a33699e85632838
katherinemurphy <16kamurphy@iaa.edu.ec>
2015-05-08 08:27:29 -0500

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,23 @@
+name = 'Katherine Murphy'
+age = 17 
+gpa = 3.7
+
+print name 
+print age
+print gpa 
+
+print 'My name is', name, ' . I am ' , age , 'years old.' 
+
+print "In ten years, I will be" , age + 10
+
+#Dog years
+ageInHumanYears = 3
+ageInDogYears = ageInHumanYears * 7 
+print 'My dog is' , ageInDogYears, 'years old in Dog years.' 
+print 'But my dog is' , ageInHumanYears, 'years old in Human Years.'
+
+#Order of Operations
+#Parenthesis, Exponents, Multiplication, Division, Addition, Subtraction
+MyNumber = 5 + 4 * 5
+print MyNumber 
+
