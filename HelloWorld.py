2d1228e3521b0f8bdbc7db9c3cd58cfcbe637854
katherinemurphy <16kamurphy@iaa.edu.ec>
2015-05-08 08:25:16 -0500

    Create HelloWorld.py

---
+++ HelloWorld.py
@@ -0,0 +1,14 @@
+print "Hello World"
+print "My name is Katherine \nThis is Python"
+print 2015 #Printing on integer
+print "2015" #Printing a string 
+print 3.1416 #Printing a floating point number 
+
+print 2 + 2 
+print 2 - 2 
+print 2 * 2 
+print 2 / 2 
+print 2 % 2
+
+print 3.1416 * 2.0 
+print 3.1416 * 2
