492bb3525921a1e12ced23250433cf8cc4300e01
katherinemurphy <16kamurphy@iaa.edu.ec>
2015-05-20 13:51:42 -0500

    Create Input.py

---
+++ Input.py
@@ -0,0 +1,8 @@
+name = raw_input ("what is your name?")
+age = input("How old are you?")
+
+print name, "you are", age, "years old"
+
+age = age * 7 
+print name, "you are", age, "years old in dog years" 
+print name * 7
