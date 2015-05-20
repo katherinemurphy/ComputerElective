a875a9e1ac4c444fd0e99c947865c827b2d64891
katherinemurphy <16kamurphy@iaa.edu.ec>
2015-05-20 13:45:10 -0500

    Create CommandLine.py

---
+++ CommandLine.py
@@ -0,0 +1,8 @@
+import sys 
+
+#print "you are using command line arguments:" , sys.argv
+#print "the first argument you entered is" , sys.argv[1]
+#print "the second argument you entered is" , sys.argv[2]
+#print "the third argument you entered is" , sys.argv[3]
+
+print "my name is" , sys.argv[1]
