370e236a1a1e156eb7b5008debc7e3979d1f5976
katherinemurphy <16kamurphy@iaa.edu.ec>
2015-05-26 08:27:21 -0500

    Create Conditional2.py

---
+++ Conditional2.py
@@ -0,0 +1,8 @@
+age = input ("What is the age of the customer? ")
+
+if age >= 21:
+    print "The customer is allowed to drink liquor"
+    print "You may serve"
+else: 
+    print "The customer is not allowed to drink liquor"
+print "End of program" 
